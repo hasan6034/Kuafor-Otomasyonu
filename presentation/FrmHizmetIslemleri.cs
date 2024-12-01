10b1a37ccf21621da31abdaa6c363051f4941f56
Caner <canerorak13@gmail.com>
2024-12-01 21:58:55 +0300

    Functions of the services button have been added and some design improvements have been made.

---
+++ presentation/FrmHizmetIslemleri.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kuafor_Otomasyonu
+{
+    public partial class FrmHizmetIslemleri : Form
+    {
+        public FrmHizmetIslemleri()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
--- presentation/frmAnaEkran.Designer.cs
+++ presentation/frmAnaEkran.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             DevExpress.XtraEditors.TileItemElement tileItemElement1 = new DevExpress.XtraEditors.TileItemElement();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAnaEkran));
             DevExpress.XtraEditors.TileItemElement tileItemElement2 = new DevExpress.XtraEditors.TileItemElement();
             DevExpress.XtraEditors.TileItemElement tileItemElement3 = new DevExpress.XtraEditors.TileItemElement();
             DevExpress.XtraEditors.TileItemElement tileItemElement4 = new DevExpress.XtraEditors.TileItemElement();
@@ -54,6 +56,8 @@
             this.tileItem_SubeBazliRapor = new DevExpress.XtraEditors.TileItem();
             this.tileGroup_Oturum = new DevExpress.XtraEditors.TileGroup();
             this.tileItem_OturumuKapat = new DevExpress.XtraEditors.TileItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tileControl_Menu
@@ -66,9 +70,11 @@
             this.tileControl_Menu.Groups.Add(this.tileGroup_Raporlar);
             this.tileControl_Menu.Groups.Add(this.tileGroup_Oturum);
             this.tileControl_Menu.Location = new System.Drawing.Point(0, 0);
+            this.tileControl_Menu.Margin = new System.Windows.Forms.Padding(4);
             this.tileControl_Menu.MaxId = 10;
             this.tileControl_Menu.Name = "tileControl_Menu";
-            this.tileControl_Menu.Size = new System.Drawing.Size(1202, 432);
+            this.tileControl_Menu.Padding = new System.Windows.Forms.Padding(21, 22, 21, 22);
+            this.tileControl_Menu.Size = new System.Drawing.Size(1402, 532);
             this.tileControl_Menu.TabIndex = 0;
             this.tileControl_Menu.Tag = "";
             // 
@@ -83,6 +89,7 @@
             // 
             this.tileItem_KullaniciIslemleri.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_KullaniciIslemleri.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image")));
             tileItemElement1.Text = "Kullanıcı İşlemleri";
             this.tileItem_KullaniciIslemleri.Elements.Add(tileItemElement1);
             this.tileItem_KullaniciIslemleri.Id = 0;
@@ -95,6 +102,7 @@
             // 
             this.tileItem_RolTanimlama.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_RolTanimlama.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image1")));
             tileItemElement2.Text = "Kullanıcı Rol Tanımları";
             this.tileItem_RolTanimlama.Elements.Add(tileItemElement2);
             this.tileItem_RolTanimlama.Id = 1;
@@ -114,6 +122,7 @@
             // 
             this.tileItem_Subeler.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_Subeler.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement3.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image2")));
             tileItemElement3.Text = "Şube İşlemleri";
             this.tileItem_Subeler.Elements.Add(tileItemElement3);
             this.tileItem_Subeler.Id = 2;
@@ -126,12 +135,14 @@
             // 
             this.tileItem_Hizmetler.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_Hizmetler.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement4.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image3")));
             tileItemElement4.Text = "Hizmet İşlemleri";
             this.tileItem_Hizmetler.Elements.Add(tileItemElement4);
             this.tileItem_Hizmetler.Id = 3;
             this.tileItem_Hizmetler.ItemSize = DevExpress.XtraEditors.TileItemSize.Wide;
             this.tileItem_Hizmetler.Name = "tileItem_Hizmetler";
             this.tileItem_Hizmetler.Visible = false;
+            this.tileItem_Hizmetler.ItemClick += new DevExpress.XtraEditors.TileItemClickEventHandler(this.tileItem_Hizmetler_ItemClick);
             // 
             // tileGroup_Musteri
             // 
@@ -144,6 +155,7 @@
             // 
             this.tileItem_MusteriIslemleri.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_MusteriIslemleri.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement5.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image4")));
             tileItemElement5.Text = "Müşteri İşlemleri";
             this.tileItem_MusteriIslemleri.Elements.Add(tileItemElement5);
             this.tileItem_MusteriIslemleri.Id = 4;
@@ -155,6 +167,7 @@
             // 
             this.tileItem_Randevu.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_Randevu.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement6.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image5")));
             tileItemElement6.Text = "Randevu İşlemleri";
             this.tileItem_Randevu.Elements.Add(tileItemElement6);
             this.tileItem_Randevu.Id = 5;
@@ -174,6 +187,7 @@
             // 
             this.tileItem_MusteriRaporu.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_MusteriRaporu.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement7.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image6")));
             tileItemElement7.Text = "Müşteri Bazlı Rapor";
             this.tileItem_MusteriRaporu.Elements.Add(tileItemElement7);
             this.tileItem_MusteriRaporu.Id = 7;
@@ -185,6 +199,7 @@
             // 
             this.tileItem_PersonelBazliRapor.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_PersonelBazliRapor.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement8.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image7")));
             tileItemElement8.Text = "Personel Bazlı Rapor";
             this.tileItem_PersonelBazliRapor.Elements.Add(tileItemElement8);
             this.tileItem_PersonelBazliRapor.Id = 8;
@@ -196,6 +211,7 @@
             // 
             this.tileItem_SubeBazliRapor.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_SubeBazliRapor.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement9.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image8")));
             tileItemElement9.Text = "Şube Bazlı Rapor";
             this.tileItem_SubeBazliRapor.Elements.Add(tileItemElement9);
             this.tileItem_SubeBazliRapor.Id = 9;
@@ -213,6 +229,7 @@
             // 
             this.tileItem_OturumuKapat.AppearanceItem.Normal.Font = new System.Drawing.Font("Tahoma", 12F);
             this.tileItem_OturumuKapat.AppearanceItem.Normal.Options.UseFont = true;
+            tileItemElement10.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("resource.Image9")));
             tileItemElement10.Text = "Oturumu Kapat";
             this.tileItem_OturumuKapat.Elements.Add(tileItemElement10);
             this.tileItem_OturumuKapat.Id = 6;
@@ -220,12 +237,31 @@
             this.tileItem_OturumuKapat.Name = "tileItem_OturumuKapat";
             this.tileItem_OturumuKapat.ItemClick += new DevExpress.XtraEditors.TileItemClickEventHandler(this.tileItem_OturumuKapat_ItemClick);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(996, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Zaman Göstergesi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // frmAnaEkran
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1202, 432);
+            this.ClientSize = new System.Drawing.Size(1402, 532);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.tileControl_Menu);
+            this.IconOptions.Image = ((System.Drawing.Image)(resources.GetObject("frmAnaEkran.IconOptions.Image")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmAnaEkran";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kmu Berber Otomasyonu";
@@ -233,6 +269,7 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmAnaEkran_FormClosed);
             this.Load += new System.EventHandler(this.frmAnaEkran_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -254,5 +291,7 @@
         private DevExpress.XtraEditors.TileItem tileItem_MusteriRaporu;
         private DevExpress.XtraEditors.TileItem tileItem_PersonelBazliRapor;
         private DevExpress.XtraEditors.TileItem tileItem_SubeBazliRapor;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
